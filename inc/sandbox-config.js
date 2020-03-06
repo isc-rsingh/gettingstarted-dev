9eb38ac603b217e14f95500abf4b2c02cf01517f
Raj Singh <rajrsingh@gmail.com>
2020-03-06 13:02:59 -0500

    added action to config_info and cleanup

--- inc/sandbox-config.js
+++ inc/sandbox-config.js
@@ -1,18 +1,21 @@
-
-
 function sandbox_config_save(config_info) {
+    config_info['action'] = 'sandbox_config_cb'
+    console.log("Saving config data to wordpress via URL: ")
+    console.log(ajax_url)
     console.log(config_info)
-    $.ajax({
-        url: ajax_url, 
-        type: 'POST', 
-        data: config_info
-    })
-    .success( function(results) {
-        console.log('sandbox_config_save: user meta updated!')
-    })
-    .fail(function(data) {
-        console.log(data.responseText)
-        console.log("sandbox_config_save: request failed: ")
-        console.log(data.statusText)
+    jQuery(document).ready(function($){
+        $.ajax({
+            url: ajax_url, 
+            type: 'POST', 
+            data: config_info, 
+            success: function(results) {
+                console.log('sandbox_config_save: user meta updated!')
+            },
+            error: function(data) {
+                console.log("sandbox_config_save: request failed: ")
+                console.log("response text: " + data.responseText)
+                console.log("status text: " + data.statusText)
+            }
+        })
     })
 }
