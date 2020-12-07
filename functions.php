e7e78ffa7e68caa6c63d43237ab955f130c25e0b
Raj Singh <rajrsingh@gmail.com>
2020-12-07 12:57:10 -0500

    sandbox staging endpoint fixed

--- functions.php
+++ functions.php
@@ -1537,14 +1537,16 @@ function isc_global_vars() {
 
 	global $isc_globals;
 	$isc_globals = array(
-		'sanbox_token_service'  => 'https://lsiris.intersystems.com/try-iris/gs', //'https://lsiris.intersystems.com/test-iris/gs',
+		'sanbox_token_service'  => 'https://lsiris.intersystems.com/try-iris/gs',
 		'sso_registration_page'  => 'https://login.intersystems.com/login/SSO.UI.Register.cls?referrer=',
 		'sso_login_page'      => 'https://login.intersystems.com/oauth2/authorize?response_type=code&scope=email+profile+openid&client_id=zwdubaHB5lKWgT6JL-UAvH6T0wsDNpTlwRVBieR41C4&redirect_uri=',
 	);
 	$h = home_url();
 	// things to do if this is a development site
+	// - sandbox endpoint is test-iris instead of try-iris
+	// - SSO registration changes to URLs with uat in the endpoint
 	if ( strpos($h, 'dev-start') ) {
-		$isc_globals['sanbox_token_service']  = 'https://lsiris.intersystems.com/try-iris/gs';
+		$isc_globals['sanbox_token_service']  = 'https://lsiris.intersystems.com/test-iris/gs';
 		$isc_globals['sso_registration_page'] = 'https://login.intersystems.com/loginuat/SSO.UI.Register.cls?referrer=';
 		$isc_globals['sso_login_page'] = 'https://login.intersystems.com/uat/oauth2/authorize?response_type=code&scope=email+profile+openid&client_id=6XlAB83aJbEcrCJ4oisbRUc0elnmYtRrjXQBFX4NRlw&redirect_uri=';
 	}
