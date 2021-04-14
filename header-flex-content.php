c94f948a3994e7046ad7988b08400d69bca1bd03
Raj Singh <3768695+isc-rsingh@users.noreply.github.com>
2021-04-14 10:36:29 -0400

    add heap - commented out

--- header-flex-content.php
+++ header-flex-content.php
@@ -29,12 +29,18 @@
 <!-- ?php if ( function_exists('cn_cookies_accepted') && cn_cookies_accepted() ) : ?> -->
 <?php if ( function_exists('activate_em_cookie_notification') ) : ?>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-PKG7GB');</script>
+	})(window,document,'script','dataLayer','GTM-PKG7GB');</script> -->
 	<!-- End Google Tag Manager -->
+	<!-- Heap -->
+	<!-- <script type="text/javascript">   
+		window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};   
+		heap.load("2183293721"); 
+	</script> -->
+	<!-- End Heap -->
 <?php endif; ?>
 
 <title>
